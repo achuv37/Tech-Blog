d7fcdcbaad695dd37f02a7b0f3dbdf1c8adf838f
Achu Ajesh <achuv37@gmail.com>
2022-05-21 10:35:58 -0400

    modified timezone

--- utils/helpers.js
+++ utils/helpers.js
@@ -10,7 +10,7 @@ module.exports = {
     format_date: (date) => {
       return `${new Date(date).toLocaleDateString('en-US', {timeZone:'America/New_York'})
 
-        } ${new Date(date).getTimezoneOffset()}`;
+        }`;
 
 }
 
